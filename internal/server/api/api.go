e567d0cd6cd72fc00da5bd261352f0471f3f1e1e
v1tbrah <107151880+v1tbrah@users.noreply.github.com>
2022-07-30 16:06:30 +0300

    Increment7->8
    * Increment6->7
    
    * Increment6
    
    * increment4
    
    * increment4 (first)
    
    * returned functionality from increment2
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * for autotests
    
    * _
    
    * fsd
    
    * for autotests
    
    * ggg
    
    * last changes for increment5
    
    * increment5
    
    * increment5
    
    * increment5
    
    * increment5
    
    * fix error agent_test.go
    
    * fix options agent/server
    
    * last fix from increment5
    
    * increment6
    
    * minor fix
    
    * increment7
    
    * fix flags in agent and server
    
    * fix env parse options

--- internal/server/api/api.go
+++ internal/server/api/api.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/v1tbrah/metricsAndAlerting/internal/server/service"
@@ -13,13 +12,6 @@ import (
 	"time"
 )
 
-type options struct {
-	Addr          string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
-	StoreFile     string        `env:"STORE_FILE"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
-}
-
 type api struct {
 	server  *http.Server
 	service *service.Service
@@ -28,25 +20,33 @@ type api struct {
 
 // Creates the API.
 func NewAPI(service *service.Service) *api {
-	server := &http.Server{}
-	options := newDefaultOptions()
-	err := env.Parse(options)
-	if err != nil {
-		log.Println(err)
-	}
-	server.Addr = options.Addr
-
 	newAPI := &api{
-		server:  server,
 		service: service,
-		options: options}
+		options: newDefaultOptions()}
 
-	server.Handler = newAPI.newRouter()
+	newAPI.options.parseFromOsArgs()
+	newAPI.options.parseFromEnv()
+
+	newAPI.server = &http.Server{
+		Addr:    newAPI.options.Addr,
+		Handler: newAPI.newRouter(),
+	}
+
 	return newAPI
 }
 
-func newDefaultOptions() *options {
-	return &options{Addr: "127.0.0.1:8080"}
+func (a *api) newRouter() chi.Router {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", a.getPageHandler())
+	r.Post("/update/", a.updateJSONHandler)
+	r.Post("/value/", a.getValueJSONHandler)
+	r.Post("/update/{type}/{metric}/{val}", checkTypeAndNameMetric("update", a.updateHandler()))
+	r.Get("/value/{type}/{metric}", checkTypeAndNameMetric("value", a.getValueHandler()))
+
+	return r
 }
 
 //The API starts.
@@ -73,20 +73,8 @@ func (a *api) Run() {
 	<-exit
 	if err := a.service.SaveMetricsToFile(a.options.StoreFile); err != nil {
 		log.Println(err)
+	} else {
+		log.Println("Metrics saved in file:", a.options.StoreFile)
 	}
 	os.Exit(0)
-}
-
-func (a *api) newRouter() chi.Router {
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-
-	r.Get("/", a.getPageHandler())
-	r.Post("/update/", a.updateJSONHandler)
-	r.Post("/value/", a.getValueJSONHandler)
-	r.Post("/update/{type}/{metric}/{val}", checkTypeAndNameMetric("update", a.updateHandler()))
-	r.Get("/value/{type}/{metric}", checkTypeAndNameMetric("value", a.getValueHandler()))
-
-	return r
 }
---
+++ internal/server/api/options.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type options struct {
+	Addr          string        `env:"ADDRESS"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL"`
+	StoreFile     string        `env:"STORE_FILE"`
+	Restore       bool          `env:"RESTORE"`
+}
+
+func newDefaultOptions() *options {
+	return &options{
+		Addr:          "127.0.0.1:8080",
+		StoreInterval: time.Second * 300,
+		Restore:       true}
+}
+
+func (o *options) parseFromOsArgs() {
+	if strings.Contains(os.Args[0], ".test") {
+		return
+	}
+	defaultOptions := newDefaultOptions()
+
+	flag.StringVar(&o.Addr, "a", defaultOptions.Addr, "api server address")
+	flag.DurationVar(&o.StoreInterval, "i", defaultOptions.StoreInterval, "interval for writing metrics to a file")
+	flag.StringVar(&o.StoreFile, "f", defaultOptions.StoreFile, "path to persistent file storage")
+	flag.BoolVar(&o.Restore, "r", defaultOptions.Restore, "flag for loading metrics from a file at the start of the api")
+
+	flag.Parse()
+}
+
+func (o *options) parseFromEnv() {
+	err := env.Parse(o)
+	if err != nil {
+		log.Println(err)
+	}
+}
