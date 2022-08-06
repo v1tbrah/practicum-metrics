5e4097e15a362d7e13603cd9f8a854a1ad955a04
v1tbrah <khatibvictor97@gmail.com>
2022-08-06 22:01:40 +0300

    fix errors #3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,9 @@ import (
 	"github.com/v1tbrah/metricsAndAlerting/internal/server/config"
 	"github.com/v1tbrah/metricsAndAlerting/internal/server/repo/memory"
 	"github.com/v1tbrah/metricsAndAlerting/internal/server/service"
-	"github.com/v1tbrah/metricsAndAlerting/pkg/metric"
 )
 
 func main() {
-
-	thisMetric := metric.Metrics{MType: "gauge"}
-	thisMetric.UpdateHash("fsdfds")
 
 	myData := memory.NewStorage()
 	myCfg := config.NewCfg(config.WithFlag, config.WithEnv)
